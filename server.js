e3b0b55cc3e1666e11aabf9bdec844839cefc133
SurionA <frere.maxime@gmail.com>
2016-07-29 21:31:38 +0200

    [ADD] POST temp

--- server.js
+++ server.js
@@ -5,10 +5,30 @@ var serial = new SerialPort('/dev/cu.usbmodem1411', {
   baudrate: 115200,
 });
 
+var request = require('request');
+var rand = Math.floor(Math.random()*100000000).toString();
+
 // Add data read event listener
 serial.on('data', function (chunk) {
 
   var data = JSON.parse(chunk);
+  var options = {
+    uri: 'http://127.0.0.1:9191/api/hydrometries',
+    method: 'POST',
+    json: {
+      room: "5793cd567899cf0410b8bbb9",
+      temperature: data.temperature,
+      humidity: data.humidity
+    }
+  };
+  request.post(options, function (error, response, body) {
+    if(response.statusCode == 201){
+      console.log('document saved as: http://127.0.0.1:9191/api/hydrometries');
+    } else {
+      console.log('error: '+ response.statusCode);
+      console.log(body);
+    }
+  });
   console.log('temperature: ', data.temperature);
   console.log('humidity: ', data.humidity);
 });
