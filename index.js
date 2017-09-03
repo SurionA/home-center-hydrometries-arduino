05cf080447273cca74856fa279c8946bb766b33c
SurionA <frere.maxime@gmail.com>
2017-09-03 15:46:35 +0200

    #UPDATE# expose handlers

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const connector = require('./lib/connector');
+const handlers = require('./lib/handlers/hydrometries');
 
 module.exports = {
+    get: handlers.get,
     initSocket: connector.socket.init,
+    save: handlers.save,
 };
